b316cc2da9eee988e789ab4c92c58e5aae2e1b4b
rodrigofmeneses <rodrigo.menesesufc@gmail.com>
2023-01-10 16:36:50 -0300

    build: :rocket: Expose HOST to 0.0.0.0

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-import app from "./src/app.js"
+import app from "./src/app.js";
 
 const PORT = process.env.PORT || 3000
+const HOST = '0.0.0.0';
 
-app.listen(PORT, () => {
+
+app.listen(PORT, HOST, () => {
     console.log(`Servidor escutando em http://localhost:${PORT}`)
 })
